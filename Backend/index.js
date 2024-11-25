20fe731cee723f81d0cb8312e446fd32fd0b9463
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2024-11-25 23:33:53 +0530

    basic folder setup

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const rootRouter = require("./routes/index")
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/v1",rootRouter)
 
-const router = express.Router();
-
-module.exports = router;
+app.listen(3000);
